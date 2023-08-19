10bba44e146a1d559445ecccb9d4fbc98aebe93d
ANIL-BUDAMAKUNTLA <111275389+ANIL-BUDAMAKUNTLA@users.noreply.github.com>
2023-08-19 12:27:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ volumeSlider = document.querySelector(".volume-slider input"),
 keysCheckbox = document.querySelector(".keys-checkbox input");
 
 let allKeys = [],
-audio = new Audio("tunes/a.wav"); // by default, audio src is "a" tune
+audio = new Audio("tunes/a.wav"); 
 
 const playTune = (key) => {
-    audio.src = `tunes/${key}.wav`; // passing audio src based on key pressed 
-    audio.play(); // playing audio
+    audio.src = `tunes/${key}.wav`; 
+    audio.play(); 
 
     const clickedKey = document.querySelector(`[data-key="${key}"]`); // getting clicked key element
     clickedKey.classList.add("active"); // adding active class to the clicked key element
